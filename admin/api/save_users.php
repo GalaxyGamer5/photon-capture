8f70ad2cdec08cac372ecbf5a99b934c01f51689
GalaxyGamer5 <82287934+GalaxyGamer5@users.noreply.github.com>
2025-12-11 14:33:39 +0100

    Add files via upload

--- admin/api/save_users.php
+++ admin/api/save_users.php
@@ -1,6 +1,14 @@
 <?php
 // admin/api/save_users.php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // Get JSON input
 $input = file_get_contents('php://input');
