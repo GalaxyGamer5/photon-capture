e0a08f9e8d9083047cad15c432bc5526b0036bc0
GalaxyGamer5 <jonathanweidner05@gmail.com>
2026-03-23 13:44:09 +0100

    a

--- admin/api/manage-pricing.php
+++ admin/api/manage-pricing.php
@@ -14,7 +14,7 @@ $jsonData = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
 if (file_put_contents($file, $jsonData)) {
     $firstTitle = isset($data['packages'][0]['title']['de']) ? $data['packages'][0]['title']['de'] : 'N/A';
-    echo json_encode(['success' => true, 'bytes' => strlen($jsonData), 'received_first' => $firstTitle]);
+    echo json_encode(['success' => true, 'bytes' => strlen($jsonData), 'received_first' => $firstTitle, 'path' => realpath($file)]);
 } else {
     echo json_encode(['success' => false, 'error' => 'Failed to write pricing data to ' . $file]);
 }
